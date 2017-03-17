3d13653fba2787b2f7c2ccdf35b4d03b68b3e281
qianpeili <qianpeili@bianfeng.com>
2017-03-17 11:44:01 +0800

    first src

---
+++ src/GuessGame/GameLauncher.java
@@ -0,0 +1,11 @@
+package GuessGame;
+
+/**
+ * Created by qianpeili on 2017/3/13.
+ */
+public class GameLauncher {
+    public static void main (String [] args) {
+        GuessGame game = new GuessGame();
+        game.startGame();
+    }
+}
